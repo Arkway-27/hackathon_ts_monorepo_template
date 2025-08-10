df036f67656f7128b82d137eaa4ff97039860c9c
Athul Anoop <athulanoop2020@gmail.com>
2025-08-10 19:45:32 +0530

    update: include sequelize integration for nestjs

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
-    imports: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: "postgres",
+            host: process.env.POSTGRES_HOST,
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            // synchronize: process.env.NODE_ENV !== "production",
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
